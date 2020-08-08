ac36ef3b39d435ed5f7e89239c4295728b3d1772
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2020-08-08 12:34:58 +0700

    Update Vevoast.py

--- Vevoast.py
+++ Vevoast.py
@@ -1,29 +1,29 @@
 import os
 import time
 import calendar
-import win32com.client as wincl
-import pyaudio
-import googlesearch
-import wikipedia
-import speech_recognition as sr
+#import win32com.client as wincl
+#import pyaudio
+#import googlesearch
+#import wikipedia
+#import speech_recognition as sr
 #from PIL import Image
-from googlesearch import search
-speak = wincl.Dispatch("SAPI.SpVoice")
-r = sr.Recognizer()
+#from googlesearch import search
+#speak = wincl.Dispatch("SAPI.SpVoice")
+#r = sr.Recognizer()
 a = "hello"
 face = 0
 take = 0
 mode = "off"
-os.system('cls')
+os.system('clear')
 print("What is your name?")
-speak.Speak("What is your name?")
+os.system('say What is your name?')
 name = input()
 print("hello " + name)
-speak.Speak("hello" + name)
+os.system('say hello' + name)
 print("How old are you?")
-speak.Speak("How old are you?")
+os.system('say How old are you?')
 old = input()
-os.system('cls')
+os.system('clear')
 print("type to chat or say to chat:")
 run = True
 while run:
@@ -34,6 +34,7 @@ while run:
         print("@_@")
     if face == "sad":
         print(":(")
+    '''
     if mode == "on":
         with sr.Microphone() as source:
             print("Speak:")
@@ -53,7 +54,7 @@ while run:
                 mode = "on"
                 speak.Speak("voice on")
                 run = True
-                '''
+            ''
             if "google" in you or "search" in you:
                 with sr.Microphone() as source:
                     print("search:")
@@ -69,7 +70,7 @@ while run:
                         speak.Speak("i don't understand?")
                         run = True
                     run = True
-                    '''
+                    ''
             if "wikipedia" in you or "wiki" in you:
                 with sr.Microphone() as source:
                     print("search:")
@@ -87,16 +88,19 @@ while run:
                         speak.Speak("i don't understand?")
                         run = True
                     run = True
-    elif mode == "off":
+    '''
+    if mode == "off":
         you = input()
     if "voice" in you and "off" in you:
         mode = "off"
-        speak.Speak("voice off")
+        os.system('say voice off')
         run = True
+    '''
     if "voice" in you and "on" in you:
         mode = "on"
-        speak.Speak("voice on")
+        os.system('say voice on')
         run = True
+    '''
     if "search" in you or "google" in you:
         if mode == "on":
             with sr.Microphone() as source:
@@ -116,22 +120,22 @@ while run:
                 run = True
     if a in you:
         print("hello " + name)
-        speak.Speak("hello " + name)
+        os.system('say hello ' + name)
         run = True
     if "feel" in you:
         face = "happy"
         print("i'm good")
-        speak.Speak("i'm good")
+        os.system('say i am good')
         run = True
     if "your name" in you or "who are you" in you:
         face = "happy"
         print("my name is Vector")
-        speak.Speak("my name is Vector")
+        os.system('say my name is Vector')
         run = True
     if "my" in you and "name" in you:
         face = "happy"
         print("your name is " + name)
-        speak.Speak("your name is " +name)
+        os.system('say your name is ' +name)
         run = True
     if "good" in you:
         face = "happy"
@@ -139,29 +143,29 @@ while run:
     if "bad" in you:
         face = "sad"
         print("boo!")
-        speak.Speak("boo!")
+        os.system('say boo!')
         run = True
     if "old" in you and "you" in you:
         face = "happy"
         print("i don't have old!")
-        speak.Speak("i don't have old!")
+        os.system('say i don t have old!')
         run = True
     if "old" in you and "am" in you and "i" in you:
         face = "happy"
         print("you are " + old + " year old" )
-        speak.Speak("you are" + old + "year old")
+        os.system('say you are' + old + 'say year old')
         run = True
     if "wikipedia" in you:
         print("search:")
         take = input()
         wiki = wikipedia.summary(take, sentences=5)
         print(wiki)
-        speak.Speak(wiki)
+        os.system('say ' + wiki)
         run = True
     if "bye" in you or "exit" in you or "stop" in you or "close" in you:
         face = "happy"
         print("bye")
-        speak.Speak("bye")
+        os.system('say bye')
         time.sleep(2)
         run = False
     if "time" in you:
@@ -176,19 +180,19 @@ while run:
         mm = thang
         print("Time:")
         print(gio, phut)
-        speak.Speak(gio, phut)
+        #os.system('say ' + gio, phut)
         run = True
     if "date" in you:
-        print(calendar.month(yy, mm))
+        os.system('cal')
         run = True
     if "tell me a joke" in you:
         print("i'm not an A.I")
-        speak.Speak("i'm not an A.I")
+        os.system('say i am not an A.I')
         run = True
     if "who create you" in you:
         os.system('cls')
         print("i'm make by johnny20061234 the ideal by phucxo")
-        speak.Speak("i'm make by johnny20061234 the ideal by phucxo")
+        os.system('say i am make by johnny20061234 the ideal by phucxo')
         f = open("create.txt", "r")
         print(f.read())
         time.sleep(5)
@@ -197,72 +201,72 @@ while run:
     if "favorite food" in you:
         face = "happy"
         print("my favorite food is iron")
-        speak.Speak("my favorite food is iron")
+        os.system('say my favorite food is iron')
         run = True
     if "favorite drinks" in you:
         face = "happy"
         print("my favorite drinks is soda")
-        speak.Speak("my favorite drinks is soda")
+        os.system('say my favorite drinks is soda')
         run = True
     if "favorite school" in you:
         face = "happy"
         print("my favorite school is Assistant school")
-        speak.Speak("my favorite school is Assistant school")
+        os.system('say my favorite school is Assistant school')
         run = True
     if "wear" in you:
         face = "happy"
         print("I like to wear leather")
-        speak.Speak("I like to wear leather")
+        os.system('say I like to wear leather')
         run = True
     if "Vector" in you or "vector" in you:
         face = "happy"
         print("i'm here")
-        speak.Speak("i'm here")
+        os.system('say i am here')
         run = True
     if "have an error" in you or "error" in you and "you" in you:
         face = "sad"
         print("yes i have an error is my name")
-        speak.Speak("yes i have an error is my name")
+        os.system('say yes i have an error is my name')
         run = True
     if "do" in you and "hack" in you:
         face = "happy"
         print("no i'm not been hack")
-        speak.Speak("no i'm not been hack")
+        os.system('say no i am not been hack')
         run = True
     if "where" in you and "you" in you and "form" in you:
         face = "happy"
         print("i form Vietnam")
-        speak.Speak("i form Vietnam")
+        os.system('say i form Vietnam')
         run = True
     if "where" in you and "you" in you and "live":
         face = "happy"
         print("i live in Vietnam sever")
-        speak.Speak("i live in Vietnam sever")
+        os.system('say i live in Vietnam sever')
         run = True
     if "favorite color" in you or "color" in you and "like" in you:
         face = "happy"
         print("my favorite colors is red and green")
-        speak.Speak("my favorite colors is red and green")
+        os.system('say my favorite colors is red and green')
         run = True
     if "do you" in you and "like" in you or "do you" in you and "favorite" in you:
         face = "happy"
         print("yes, i like")
-        speak.Speak("yes, i like")
+        os.system('say yes, i like')
         run = True
     if "hot" in you or "cold" in you:
         face = "happy"
         print("i feel normal")
-        speak.Speak("i fell normal")
+        os.system('say i fell normal')
         run = True
     if "have" in you and "hair" in you:
         face = "no"
         print("i'm not have hair")
-        speak.Speak("i'm not have hair")
+        os.system('say i am not have hair')
         run = True
     if "connect" in you and "internet" in you:
         face = "happy"
         print("i can connect you can search google or wikipedia by say it")
-        speak.Speak("i can connect you can search google or wikipedia by say it")
+        os.system('say i can connect you can search google or wikipedia by say it')
         run = True
     if "clear" in you or "clean" in you:
         os.system('cls')
@@ -270,7 +274,7 @@ while run:
     if "love" in you and "me" in you:
         face = "happy"
         print("Maybe")
-        speak.Speak("Maybe")
+        os.system('say Maybe')
         import webbrowser; webbrowser.open('anime_lol.gif')
         '''
         im = Image.open("anime_lol.gif")
@@ -280,8 +284,6 @@ while run:
         im.save("frame1.png")
         im.seek(2)
         im.save("frame2.png")
-        '''
-        '''
         animation = pyglet.image.load_animation('anime_lol.gif')
         animSprite = pyglet.sprite.Sprite(animation)
         
@@ -306,9 +308,28 @@ while run:
         print("file to open")
         try:
             fileopen = input()
-            speak.Speak("open " + fileopen)
-            os.startfile(fileopen)
+            os.system('say open ' + fileopen)
+            os.system('open ' + fileopen)
         except:
             print("error program or file or folder not exist")
-            speak.Speak("error program or file or folder not exist")
+            os.system('say error program or file or folder not exist')
+    if "laund pad" in you:
+        os.system('say open laund pad')
+        os.system('open /Applications/Launchpad.app')
+        run = True
+    if "note stick" in you:
+        os.system('say open note stick')
+        os.system('open /Applications/Stickies.app')
+        run - True
+    if "dashboard" in you or "board" in you:
+        os.system('say open Dashboard')
+        os.system('open /Applications/Dashboard.app')
+        run = True
+    if "chess" in you:
+        os.system('say open chess')
+        os.system('open /Applications/Chess.app')
+        run = True
+    if "calculator" in you:
+        os.system('say open calculator')
+        os.system('open /Applications/Calculator.app')
         run = True
