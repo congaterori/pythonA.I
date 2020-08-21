115d2b37bb645779a10b0dae6ecf80cc2a344ca1
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2020-08-21 17:48:09 +0700

    Update setup voice A.I.bat

--- setup voice A.I.bat
+++ setup voice A.I.bat
@@ -4,6 +4,7 @@ cls
 color a
 msg * Please install Python first
 msg * Don't close setup file
+pip install --upgrade pip
 start pip install pyaudio
 start pip install pipwin
 start pipwin install pyaudio
@@ -13,6 +14,8 @@ start pip install SpeechRecognition
 start pip install google
 start pip install wikipedia
 start pip install requests
+start pip install wolframalpha
+start pip install wolframalpha api
 timeout 5
 echo if done all click button enter
 pause
