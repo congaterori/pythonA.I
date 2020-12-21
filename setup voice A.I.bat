71f1c079c1feb1cea77987645c09912b052ecb1b
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2020-12-21 21:30:49 +0700

    Update setup voice A.I.bat

--- setup voice A.I.bat
+++ setup voice A.I.bat
@@ -4,17 +4,25 @@ cls
 color a
 msg * Please install Python first
 msg * Don't close setup file
+pip install --upgrade pip
+pip install wheel
 start pip install pyaudio
 start pip install pipwin
 start pipwin install pyaudio
+start pip install pypiwin32
 start pip install pywin32
 start pip install pypiwn32
+start pip install textblob
 start pip install SpeechRecognition
 start pip install google
 start pip install wikipedia
 start pip install requests
 start pip install wolframalpha
 start pip install wolframalpha api
+start pip install akinator.py
+start pip install aiohttp
+start pip install aiodns
+start pip install cchardet
 timeout 5
 echo if done all click button enter
 pause
