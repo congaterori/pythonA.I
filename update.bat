78d0d8aecba052da6a08cda4f04c89e512639e67
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2021-03-31 19:32:05 +0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -63,23 +63,25 @@ if "%version%" == "10.0" goto win10download
 ::Del up.bat
 ::cd %CD%
 :win7download
-echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/congaterori/pythonA.I/archive/refs/heads/beta-for-windows-10.zip', 'pythonA.I-master.zip')" > up.bat
+echo @echo off > up.bat
+echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/congaterori/pythonA.I/archive/refs/heads/beta-for-windows-10.zip', 'pythonA.I-master.zip')" >> up.bat
 echo unzip.exe -o pythonA.I-master.zip > nul >> up.bat
 echo timeout 2 > nul >> up.bat
 echo Del pythonA.I-master.zip >> up.bat
 echo Del unzip.exe >> up.bat
-echo start Del up.bat & exit >> up.bat
+echo (goto) 2>nul & del "%~f0" >> up.bat
 start up.bat
 cd %now%
 endlocal
 exit
 :win10download
-echo powershell -Command "Invoke-WebRequest https://github.com/congaterori/pythonA.I/archive/refs/heads/beta-for-windows-10.zip -OutFile pythonA.I-master.zip" > up.bat
+echo @echo off > up.bat
+echo powershell -Command "Invoke-WebRequest https://github.com/congaterori/pythonA.I/archive/refs/heads/beta-for-windows-10.zip -OutFile pythonA.I-master.zip" >> up.bat
 echo unzip.exe -o pythonA.I-master.zip > nul >> up.bat
 echo timeout 2 > nul >> up.bat
 echo Del pythonA.I-master.zip >> up.bat
 echo Del unzip.exe >> up.bat
-echo start Del up.bat & exit >> up.bat
+echo (goto) 2>nul & del "%~f0" >> up.bat
 start up.bat
 cd %now%
 endlocal
