1e06b1b67ef9c483b8e9e2210a585b6f3f2d9cc5
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2021-03-31 19:11:59 +0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -64,10 +64,12 @@ if "%version%" == "10.0" goto win10download
 ::cd %CD%
 :win7download
 echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/congaterori/pythonA.I/archive/refs/heads/beta-for-windows-10.zip', 'pythonA.I-master.zip')" > up.bat
-echo unzip.exe pythonA.I-master.zip > nul >> up.bat
+echo unzip.exe -f pythonA.I-master.zip > nul >> up.bat
 echo timeout 5 > nul >> up.bat
 echo Del pythonA.I-master.zip >> up.bat
 start up.bat
+pause
+Del up.bat
 cd %now%
 endlocal
 exit
@@ -78,6 +80,8 @@ echo timeout 5 > nul >> up.bat
 echo Del pythonA.I-master.zip >> up.bat
 echo Del unzip.exe >> up.bat
 start up.bat
+pause
+Del up.bat
 cd %now%
 endlocal
 exit
